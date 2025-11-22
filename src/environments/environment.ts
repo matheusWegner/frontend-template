846536a76f421a8e7835dacb66ba9bd16ec64ede
matheusWegner <matheus.christo@dbfactory.com.br>
2025-11-22 19:14:11 -0300

    ajustando url backen

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   appUrl: 'https://vsddwpmyga.us-west-2.awsapprunner.com',
-  apiBaseUrl: 'https://vsddwpmyga.us-west-2.awsapprunner.com/api',
+  apiBaseUrl: 'https://fbf2vdjpf3.us-east-1.awsapprunner.com',
   keycloak: {
     url: 'https://k4q2xqmsnc.us-east-1.awsapprunner.com',
     realm: 'app',
