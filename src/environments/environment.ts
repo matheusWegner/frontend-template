4f84404b82e591bd8e54cd5360c857b992963aec
Matheus Wegner <matheus wegner>
2025-12-07 11:42:16 -0300

    fix: ajustando urls

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  appUrl: 'https://vsddwpmyga.us-west-2.awsapprunner.com',
-  apiBaseUrl: 'https://fbf2vdjpf3.us-east-1.awsapprunner.com',
+  appUrl: 'https://tum2kqzzth.us-west-2.awsapprunner.com',
+  apiBaseUrl: 'https://sidgxrthy2.us-east-1.awsapprunner.com',
   keycloak: {
-    url: 'https://k4q2xqmsnc.us-east-1.awsapprunner.com',
+    url: 'https://ptjmue3upq.us-east-1.awsapprunner.com',
     realm: 'app',
     clientId: 'frontend-spa',
     scope: ''
